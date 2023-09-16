8e9834b07a9a0b8c81d38e340b36f114b8c5d6a2
tejaspachgade2315 <tejaspachgade2315@gmail.com>
2023-09-17 00:53:14 +0530

    Deployment

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -10,7 +10,7 @@ function Login(){
     axios.defaults.withCredentials = true;
     const handleSubmit=(e)=>{
         e.preventDefault()
-        axios.post('https://login-register-mern-backend.vercel.app/login',{email,password})
+        axios.post('https://login-register-mern-frontend.vercel.app/login',{email,password})
         .then(result=>{
             console.log(result)
             if(result.data==="Success"){
