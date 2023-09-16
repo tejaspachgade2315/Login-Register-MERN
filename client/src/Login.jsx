180ff1dce88e62369bc69df6f8c0e236037696ba
tejaspachgade2315 <tejaspachgade2315@gmail.com>
2023-09-17 00:50:03 +0530

    Changes

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -7,10 +7,10 @@ function Login(){
     const [email,setEmail]=useState();
     const [password,setPassword]=useState();
     const navigate=useNavigate();
-
+    axios.defaults.withCredentials = true;
     const handleSubmit=(e)=>{
         e.preventDefault()
-        axios.post('http://localhost:3001/login',{email,password})
+        axios.post('https://login-register-mern-backend.vercel.app/login',{email,password})
         .then(result=>{
             console.log(result)
             if(result.data==="Success"){
